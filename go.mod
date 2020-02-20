6f3ea2cfb106628e78a762edd1b02f6d14a57cdb
quique <4859986-pragmaticreviews@users.noreply.gitlab.com>
2020-02-20 19:51:15 -0300

    Adding middlewares: BasicAuth, Logger and Debugging

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gitlab.com/pragmaticreviews/golang-gin-poc
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/tpkeeper/gin-dump v1.0.0
+)
--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/middlewares"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
 )
 
@@ -11,8 +16,19 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(),
+		middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
