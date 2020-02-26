9e793410e6194fdd142fe4ddbe5c36baec8e4c69
quique <4859986-pragmaticreviews@users.noreply.gitlab.com>
2020-02-26 18:29:22 -0300

    Adding changes made in the video series.

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -4,18 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/entity"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/validators"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 type VideoController interface {
 	FindAll() []entity.Video
-	Save(ctx *gin.Context) entity.Video
+	Save(ctx *gin.Context) error
 }
 
 type controller struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
 	return &controller{
 		service: service,
 	}
@@ -25,9 +31,16 @@ func (c *controller) FindAll() []entity.Video {
 	return c.service.FindAll()
 }
 
-func (c *controller) Save(ctx *gin.Context) entity.Video {
+func (c *controller) Save(ctx *gin.Context) error {
 	var video entity.Video
-	ctx.BindJSON(&video)
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
+	if err != nil {
+		return err
+	}
 	c.service.Save(video)
-	return video
+	return nil
 }
