4ede1936620288e0faec6b1bb8b69d9cf0fd37bf
quique <4859986-pragmaticreviews@users.noreply.gitlab.com>
2020-03-09 21:49:52 -0300

    Breaking the compile stage (01)

--- service/video-service.go
+++ service/video-service.go
@@ -2,7 +2,7 @@ package service
 
 import "gitlab.com/pragmaticreviews/golang-gin-poc/entity"
 
-type VideoService interface {
+type Video Service interface {
 	Save(entity.Video) entity.Video
 	FindAll() []entity.Video
 }
